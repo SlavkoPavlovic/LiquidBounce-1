0d61249308cd417a46a412441dd4fc240d7cbc29
Slavko <33180175+SlavkoPavlovic@users.noreply.github.com>
2021-01-11 16:27:20 +0100

    Ignore tools with low durability
    If enabled: Ignores tools with same or lower durability than user defined threshold.

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/player/AutoTool.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/player/AutoTool.kt
@@ -5,30 +5,40 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.player
 
-import net.ccbluex.liquidbounce.api.minecraft.util.WBlockPos
 import net.ccbluex.liquidbounce.event.ClickBlockEvent
 import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.minecraft.util.BlockPos
+import net.minecraft.item.ItemStack
+import net.ccbluex.liquidbounce.value.IntegerValue
+import net.ccbluex.liquidbounce.value.BoolValue
 
 @ModuleInfo(name = "AutoTool", description = "Automatically selects the best tool in your inventory to mine a block.", category = ModuleCategory.PLAYER)
 class AutoTool : Module() {
+
+	private val ignoreLowDurability = BoolValue("IgnoreLowDurability", false)
+	private val lowDurabilityThreshold = IntegerValue("Threshold", 50, 1, 100)
 
     @EventTarget
     fun onClick(event: ClickBlockEvent) {
         switchSlot(event.clickedBlock ?: return)
     }
 
-    fun switchSlot(blockPos: WBlockPos) {
+    fun switchSlot(blockPos: BlockPos) {
         var bestSpeed = 1F
         var bestSlot = -1
 
-        val blockState = mc.theWorld!!.getBlockState(blockPos)
+        val block = mc.theWorld.getBlockState(blockPos).block
 
         for (i in 0..8) {
-            val item = mc.thePlayer!!.inventory.getStackInSlot(i) ?: continue
-            val speed = item.getStrVsBlock(blockState)
+            val item = mc.thePlayer.inventory.getStackInSlot(i) ?: continue
+            
+			if (ignoreLowDurability.get() && getDurability(item) <= lowDurabilityThreshold.get())
+				continue
+			
+			val speed = item.getStrVsBlock(block)
 
             if (speed > bestSpeed) {
                 bestSpeed = speed
@@ -37,7 +47,10 @@ class AutoTool : Module() {
         }
 
         if (bestSlot != -1)
-            mc.thePlayer!!.inventory.currentItem = bestSlot
+            mc.thePlayer.inventory.currentItem = bestSlot
     }
-
+	
+	fun getDurability(item: ItemStack):Int {
+		return item.getMaxDamage() - item.getItemDamage()
+	}
 }
